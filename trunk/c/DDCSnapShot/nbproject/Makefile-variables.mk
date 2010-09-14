0ba19fdab37c070a3cf9988216729b2ea14dfc63
andersondonda <andersondonda@841c0a53-1839-910e-5a6b-0b187a23d396>
2010-09-14 17:47:32 +0000

    Import DDCSnapShot

---
+++ trunk/c/DDCSnapShot/nbproject/Makefile-variables.mk
@@ -0,0 +1,32 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=ddcsnapshot
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ddcsnapshot
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=ddcsnapshot.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ddcsnapshot.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=ddcsnapshot
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/ddcsnapshot
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=ddcsnapshot.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/ddcsnapshot.tar
+# AInvestLib configuration
+CND_PLATFORM_AInvestLib=GNU-Linux-x86
+CND_ARTIFACT_DIR_AInvestLib=dist/AInvestLib/GNU-Linux-x86
+CND_ARTIFACT_NAME_AInvestLib=ddcsnapshot
+CND_ARTIFACT_PATH_AInvestLib=dist/AInvestLib/GNU-Linux-x86/ddcsnapshot
+CND_PACKAGE_DIR_AInvestLib=dist/AInvestLib/GNU-Linux-x86/package
+CND_PACKAGE_NAME_AInvestLib=ddcsnapshot.tar
+CND_PACKAGE_PATH_AInvestLib=dist/AInvestLib/GNU-Linux-x86/package/ddcsnapshot.tar
