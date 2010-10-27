af27dc0b1492f6b0194ae140846c5b64dd007ebc
andersondonda <andersondonda@841c0a53-1839-910e-5a6b-0b187a23d396>
2010-10-27 05:02:21 +0000

    Adicionado FrmSheet e FrmOpenSheet. Adicionado novas consts na unit UConsts

--- delphi/ARouter2/Project Files/ARouter2.dpr
+++ delphi/ARouter2/Project Files/ARouter2.dpr
@@ -5,7 +5,9 @@ uses
   UFrmMain in 'Units\UFrmMain.pas' {FrmMain},
   UConsts in 'Units\UConsts.pas',
   UDataControl in 'Units\UDataControl.pas',
-  UFrmConnectionControl in 'Units\UFrmConnectionControl.pas' {FrmConnectionControl};
+  UFrmConnectionControl in 'Units\UFrmConnectionControl.pas' {FrmConnectionControl},
+  UFrmSheet in 'Units\UFrmSheet.pas' {FrmSheet},
+  UFrmOpenSheet in 'Units\UFrmOpenSheet.pas' {FrmOpenSheet};
 
 {$R *.res}
 
@@ -14,5 +16,7 @@ begin
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFrmMain, FrmMain);
   Application.CreateForm(TFrmConnectionControl, FrmConnectionControl);
+  Application.CreateForm(TFrmSheet, FrmSheet);
+  Application.CreateForm(TFrmOpenSheet, FrmOpenSheet);
   Application.Run;
 end.
---
+++ delphi/ARouter2/Project Files/Units/UFrmOpenSheet.pas
@@ -0,0 +1,67 @@
+
+{$WARNINGS ON}
+{$HINTS ON}
+{$WARN UNIT_PLATFORM OFF}
+{$WARN SYMBOL_PLATFORM OFF}
+unit UFrmOpenSheet;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons, ExtCtrls, FileCtrl;
+
+type
+  TFrmOpenSheet = class(TForm)
+    FileListBox1: TFileListBox;
+    Panel1: TPanel;
+    btnOpen: TBitBtn;
+    btnCancel: TBitBtn;
+    btnDelete: TBitBtn;
+    procedure btnOpenClick(Sender: TObject);
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnDeleteClick(Sender: TObject);
+    procedure FileListBox1DblClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmOpenSheet: TFrmOpenSheet;
+
+implementation
+
+uses UFrmSheet, UConsts;
+
+{$R *.dfm}
+
+procedure TFrmOpenSheet.btnOpenClick(Sender: TObject);
+begin
+  FrmSheet.Load(FileListBox1.FileName);
+  Self.Close;
+end;
+
+procedure TFrmOpenSheet.btnCancelClick(Sender: TObject);
+begin
+ Self.Close;
+end;
+
+procedure TFrmOpenSheet.btnDeleteClick(Sender: TObject);
+begin
+ if FrmSheet.SheetName <> ExtractFileName(FileListBox1.FileName) then
+ DeleteFile(FileListBox1.FileName)
+ else
+ MessageDlg(SOpenSheet_MsgDlg_CantDeleteSheetOpened, mtInformation, [mbOk], 0);
+
+ FileListBox1.Update;
+end;
+
+procedure TFrmOpenSheet.FileListBox1DblClick(Sender: TObject);
+begin
+ if FileListBox1.SelCount=1 then
+ btnOpenClick(Self);
+end;
+
+end.
